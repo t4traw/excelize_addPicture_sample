4a90718c44c667073162794591565d1051d38973
t4traw <t4traw@gmail.com>
2023-05-18 11:39:09 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	_ "image/jpeg"
+	_ "image/png"
+
+	"github.com/xuri/excelize/v2"
+)
+
+func main() {
+	xlsx, _ := excelize.OpenFile("template.xlsx")
+	pos, _ := xlsx.SearchSheet("Sheet1", "[photo]")
+	graphicOptions := excelize.GraphicOptions{
+		AutoFit:         true,
+		LockAspectRatio: true,
+	}
+	xlsx.AddPicture("Sheet1", pos[0], "400x300.jpg", &graphicOptions)
+	xlsx.SaveAs("output.xlsx")
+}
